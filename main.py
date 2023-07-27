f01d4b769a2b6e83ba13d708f1a2fc15cab30d2d
Ebin <ebin721@gmail.com>
2023-07-27 12:46:31 +0530

    deploy_first

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
@@ -24,18 +24,41 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 ##CONFIGURE TABLES
 
 class BlogPost(db.Model):
     __tablename__ = "blog_posts"
     id = db.Column(db.Integer, primary_key=True)
-    author = db.Column(db.String(250), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
     title = db.Column(db.String(250), unique=True, nullable=False)
     subtitle = db.Column(db.String(250), nullable=False)
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+    posts = db.relationship('BlogPost', backref='author', lazy=True)
+
+    @property
+    def is_active(self):
+        return True
+
+    def get_id(self):
+        return str(self.id)
+
+
+with app.app_context():
+    db.create_all()
 
 
 class LoginForm(FlaskForm):
@@ -58,25 +81,46 @@ def get_all_posts():
     return render_template("index.html", all_posts=posts)
 
 
-@app.route('/register')
+@app.route('/register', methods=['POST', 'GET'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        hash_password = generate_password_hash(form.password.data)
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            flash("Email already registered, Please use another mail ID")
+            return render_template("register.html", form=form)
+
+        new_data = User(name=name, email=email, password=hash_password)
+        db.session.add(new_data)
+        db.session.commit()
+
+        posts = BlogPost.query.all()
+        return render_template("index.html", all_posts=posts)
     return render_template("register.html", form=form)
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return BlogPost.get(user_id)
 
-
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     form = LoginForm()
-    return render_template("login.html", form=form)
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('get_all_posts'))
+        else:
+            flash('Invalid email or password. Please try again.', 'danger')
+    return render_template('login.html', form=form)
 
 
 @app.route('/logout')
 def logout():
+    logout_user()
     return redirect(url_for('get_all_posts'))
 
 
@@ -91,12 +135,27 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=['POST', 'GET'])
 def contact():
+    if request.method == 'POST':
+        print("##########   hi  ############")
+        name = request.form.get("name")
+        email = request.form.get("email")
+        phone = request.form.get("phone")
+        message = request.form.get("message")
+
+        flash("Your message has been received. Thank you for getting in touch!")
+
+        print(f"Name: {name}")
+        print(f"Email: {email}")
+        print(f"Phone: {phone}")
+        print(f"Message: {message}")
+
     return render_template("contact.html")
 
 
 @app.route("/new-post", methods=["GET", "POST"])
+@login_required
 def add_new_post():
     form = CreatePostForm()
     if form.validate_on_submit():
@@ -105,7 +164,7 @@ def add_new_post():
             subtitle=form.subtitle.data,
             body=form.body.data,
             img_url=form.img_url.data,
-            author=current_user.username,  # Use current_user.username instead of current_user
+            author_id=current_user.id,  # Use current_user.username instead of current_user
             date=date.today().strftime("%B %d, %Y")
         )
         db.session.add(new_post)
@@ -114,21 +173,25 @@ def add_new_post():
     return render_template("make-post.html", form=form)
 
 
+@login_required
 @app.route("/edit-post/<int:post_id>")
 def edit_post(post_id):
     post = BlogPost.query.get(post_id)
+
+    if post.author != current_user:
+        flash("You are not allowed to edit this post.", "danger")
+        return redirect(url_for("show_post", post_id=post.id))
+
     edit_form = CreatePostForm(
         title=post.title,
         subtitle=post.subtitle,
         img_url=post.img_url,
-        author=post.author,
         body=post.body
     )
     if edit_form.validate_on_submit():
         post.title = edit_form.title.data
         post.subtitle = edit_form.subtitle.data
         post.img_url = edit_form.img_url.data
-        post.author = edit_form.author.data
         post.body = edit_form.body.data
         db.session.commit()
         return redirect(url_for("show_post", post_id=post.id))
---
+++ manage.py
@@ -0,0 +1,27 @@
+from flask_migrate import Migrate
+from flask.cli import FlaskGroup
+from main import app, db
+
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_posts"
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+    posts = db.relationship('BlogPost', backref='author', lazy=True)
+
+
+if __name__ == '__main__':
+    cli()
